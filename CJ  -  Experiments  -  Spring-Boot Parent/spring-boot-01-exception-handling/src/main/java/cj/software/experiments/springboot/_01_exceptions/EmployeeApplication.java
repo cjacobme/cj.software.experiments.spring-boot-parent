b636c9d02b04bfcfc75bfe3d2ace62231d5d9bf2
cjacobme <cjacobme@aol.com>
2020-02-12 21:12:08 +0100

    prepares 1st sub-project

---
+++ CJ  -  Experiments  -  Spring-Boot Parent/spring-boot-01-exception-handling/src/main/java/cj/software/experiments/springboot/_01_exceptions/EmployeeApplication.java
@@ -0,0 +1,13 @@
+package cj.software.experiments.springboot._01_exceptions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeApplication
+{
+	public static void main(String[] pArgs)
+	{
+		SpringApplication.run(EmployeeApplication.class, pArgs);
+	}
+}
---
+++ CJ  -  Experiments  -  Spring-Boot Parent/spring-boot-01-exception-handling/src/test/java/cj/software/experiments/springboot/_01_exceptions/EmployeeApplicationTest.java
@@ -0,0 +1,14 @@
+package cj.software.experiments.springboot._01_exceptions;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class EmployeeApplicationTest
+{
+	@Test
+	public void contextsLoads()
+	{
+
+	}
+}
