71e853f7e5c59238e8a4d64d795158ac68a955c0
cjacobme <cjacobme@aol.com>
2020-03-14 18:16:24 +0100

    next steps

---
+++ CJ  -  Experiments  -  Spring-Boot Parent/spring-boot-03-contract-last/src/main/java/cj/software/experiments/springboot/_03_javafirst/JavaFirstMain.java
@@ -0,0 +1,13 @@
+package cj.software.experiments.springboot._03_javafirst;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaFirstMain
+{
+	public static void main(String[] pArgs)
+	{
+		SpringApplication.run(JavaFirstMain.class, pArgs);
+	}
+}
---
+++ CJ  -  Experiments  -  Spring-Boot Parent/spring-boot-03-contract-last/src/main/java/cj/software/experiments/springboot/_03_javafirst/controller/ws/WSEndpoint.java
@@ -0,0 +1,6 @@
+package cj.software.experiments.springboot._03_javafirst.controller.ws;
+
+public interface WSEndpoint
+{
+	String NAMESPACE_URI = "http://cj.software.experiments/soap/java-first";
+}
